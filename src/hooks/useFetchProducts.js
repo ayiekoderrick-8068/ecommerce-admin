467ee7d52338e062c4b165bffe5c5257778d5b05
ayiekoderrick-8068 <ayiekoderrick@gmail.com>
2026-05-19 13:06:14 +0300

    Move coffee products into hook, remove data folder

--- src/hooks/useFetchProducts.js
+++ src/hooks/useFetchProducts.js
@@ -1,5 +1,25 @@
 import { useState, useEffect } from "react";
-import { initialProducts } from "../data/products";
+
+const initialProducts = [
+  {
+    id: "1",
+    name: "Vanilla Bean",
+    price: 10.00,
+    category: "Coffee",
+    stock: 50,
+    description: "Medium Roast, nutty flavor",
+    origin: "Columbia",
+  },
+  {
+    id: "2",
+    name: "House Blend",
+    price: 12.00,
+    category: "Coffee",
+    stock: 45,
+    description: "Dark Roast, Rich flavor",
+    origin: "Vietnam",
+  },
+];
 
 function useFetchProducts(url) {
   const [products, setProducts] = useState(initialProducts);
